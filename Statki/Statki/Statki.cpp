f2b234aab5fc427e98de3e1ea1afb7882b2c73a7
Grvy <gerwy@mailbox.org>
2021-09-23 11:30:38 +0200

    abc

--- Statki/Statki/Statki.cpp
+++ Statki/Statki/Statki.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+char statki_znaki(int i) {
+	switch (i)
+	{
+	case 0:
+		return '0';
+		break;
+	case 1:
+		return '1';
+		break;
+	default:
+		break;
+	}
+}
 
 void show(int p[][5]) {
 	cout << "   1 2 3 4 5\n  -----------\n";
@@ -10,7 +25,7 @@ void show(int p[][5]) {
 		cout << i << "| ";
 		for (int j = 0; j < 5; j++)
 		{
-			cout << p[i][j] << " ";
+			cout << statki_znaki(p[i][j]) << " ";
 		}
 		cout << endl;
 	}
@@ -18,15 +33,42 @@ void show(int p[][5]) {
 
 int main()
 {
-    int pole[5][5] = { 0 };
-	int statki[7][3];
-	int x, y, z;
-	cout << "Gdzie 3-masztowiec: ";
-	cin >> x >> y >> z;
-	if (z == 0) {
-		if (x > 3)
-			cout << "Zła lokalizacja dla x";
+	int pole[5][5] = { 0 };
+	vector<int> statki[3];
 
+	int x, y, z;
+	while (true) {
+		cout << "Gdzie 3-masztowiec: ";
+		cin >> x >> y >> z;
+
+		if (z == 0) {
+			if (x > 3)
+				cout << "Zla lokalizacja dla x\n";
+			else {
+				int p = 0;
+				for (int i = x - 1; i <= x + 2; i++) p += (pole[i][y - 1] != 0);
+				if (p == 0) break;
+			}
+		}
+		else if (z == 1) {
+			if (y > 3)
+				cout << "Zla lokazliacja dla y\n";
+			else {
+				int p = 0;
+				for (int i = y - 1; i <= y + 2; i++) p += (pole[x - 1][i] != 0);
+				if (p == 0) break;
+			}
+		}
+		else
+			break;
 	}
+	if (z == 0)
+		int id = statki[2][statki->size() - 1];
+		for (int i = x - 1; i <= x + 2; i++) {
+			statki[0].push_back(i);
+			statki[1].push_back(y);
+			//statki[2].push_back(id);
+		}
+
 	show(pole);
 }
