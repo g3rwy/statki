e9a1c484d82a4b19626461fb9922fdd9c0d3ed0a
Grvy <gerwy@mailbox.org>
2021-09-28 12:14:58 +0200

    Statki z komputerem ogarnianie

--- Statki/Statki/Statki.cpp
+++ Statki/Statki/Statki.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 
 using namespace std;
 
@@ -33,55 +34,189 @@ void umiesc_statek(int a, int plansza[7][7], vector <int> statki[3]) {
 		}
 	}
 
-	if (ok == true) {
-		id = statki[2][statki->size() - 1];
-		id++;
-		if (z == 0) {
-			for (int i = x; i < x + a; i++) {
-				statki[0].push_back(i);
-				statki[1].push_back(y);
-				statki[2].push_back(id);
+	id = statki[2][statki->size() - 1];
+	id++;
+	if (z == 0) {
+		for (int i = x; i < x + a; i++) {
+			statki[0].push_back(i);
+			statki[1].push_back(y);
+			statki[2].push_back(id);
+		}
+	}
+	else {
+		for (int i = y; i < y + a; i++) {
+			statki[0].push_back(x);
+			statki[1].push_back(i);
+			statki[2].push_back(id);
+		}
+	}
+	for (int i = 1; i <= a; i++) {
+		int xx, yy;
+		xx = statki[0][statki->size() - i];
+		yy = statki[1][statki->size() - i];
+		for (int k = xx - 1; k <= xx + 1; k++)
+			for (int l = yy - 1; l <= yy + 1; l++)
+				plansza[k][l] = 9;
+	}
+	for (int i = 1; i <= a; i++)
+		plansza[statki[0][statki->size() - i]][statki[1][statki->size() - i]] = a;
+}
+
+void umiesc_statek_k(int a, int plansza[7][7], vector <int> statki[3]) {
+
+	bool ok = false;
+	int x, y, z, id;
+
+	while (ok == false) {
+		ok = true;
+		//cout << "podaj " << a << " - masztowiec: ";
+		//cin >> x >> y >> z;
+
+		x = rand() % 5 + 1;
+		y = rand() % 5 + 1;
+		z = rand() % 2;
+
+		if (z == 0) { //poziomo
+			if (x > 6 - a) {
+				//cout << "podales zla lokalizacje dla wspolrzednej X";
+				ok = false;
 			}
+			else
+				for (int i = x; i < x + a; i++)
+					if (plansza[i][y] != 0) ok = false;
+		}
+		else if (z == 1) { //pionowo
+			if (y > 6 - a) {
+				//cout << "podales zla lokalizacje dla wspolrzednej Y";
+				ok = false;
+			}
+			else
+				for (int i = y; i < y + a; i++)
+					if (plansza[x][i] != 0) ok = false;
+		}
+	}
+
+	id = statki[2][statki->size() - 1];
+	id++; 
+
+	if (z == 0) {
+		for (int i = x; i < x + a; i++) {
+			statki[0].push_back(i);
+			statki[1].push_back(y);
+			statki[2].push_back(id);
+		}
+	}
+	else {
+		for (int i = y; i < y + a; i++) {
+			statki[0].push_back(x);
+			statki[1].push_back(i);
+			statki[2].push_back(id);
+		}
+	}
+	for (int i = 1; i <= a; i++) {
+		int xx, yy;
+		xx = statki[0][statki->size() - i];
+		yy = statki[1][statki->size() - i];
+		for (int k = xx - 1; k <= xx + 1; k++)
+			for (int l = yy - 1; l <= yy + 1; l++)
+				plansza[k][l] = 9;
+	}
+	for (int i = 1; i <= a; i++)
+		plansza[statki[0][statki->size() - i]][statki[1][statki->size() - i]] = a;
+}
+
+int strzal(int x, int y, int plansza[7][7], vector <int> statki[3]) {
+	int zat = 0,id;
+	if ((plansza[x][y] <= 3) && (plansza[x][y] >= 1)) {
+		if (plansza[x][y] == 1) {
+			plansza[x][y] = 0;
+			//zatopiony++;
+			zat = 2;
+			//cout << "zatopiony\n";
 		}
 		else {
-			for (int i = y; i < y + a; i++) {
-				statki[0].push_back(x);
-				statki[1].push_back(i);
-				statki[2].push_back(id);
+			for (int i = 1; i < statki[2].size() - 1; i++)
+				if (statki[0][i] == x && statki[1][i] == y) {
+					id = statki[2][i];
+					statki[0][i] = 0;
+					statki[1][i] = 0;
+					statki[2][i] = 0;
+					plansza[x][y] = 0;
+				}
+
+			for (int i = 1; i < statki[2].size() - 1; i++)
+				if (statki[2][i] == id) {
+					plansza[statki[0][i]][statki[1][i]]--;
+				}
+			zat = 1;
+			//cout << "trafiony\n";
+		}
+		//plansza11[x][y] = "X";
+	}
+	//else plansza11[x][y] = "O";
+	//cout << endl;
+	/*for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cout << plansza11[j][i] << " ";
+		cout << endl;
+	}
+	*/
+	return zat;
+}
+
+void wstawianie_sort(int a[][2], int n) {
+	int curr_index = 1;
+	for (int i = 0; i < n; i++) {
+		int curr_index = 0;
+		while (curr_index < n) {
+			for (int k = curr_index; k > 0; k--) {
+				if (a[k][0] > a[k - 1][0]) {
+					swap(a[k][0], a[k - 1][0]);
+					swap(a[k][1], a[k - 1][1]);
+				}
+				else break;
 			}
+			curr_index++;
 		}
-		for (int i = 1; i <= a; i++) {
-			int xx, yy;
-			xx = statki[0][statki->size() - i];
-			yy = statki[1][statki->size() - i];
-			for (int k = xx - 1; k <= xx + 1; k++)
-				for (int l = yy - 1; l <= yy + 1; l++)
-					plansza[k][l] = 9;
-		}
-		for (int i = 1; i <= a; i++)
-			plansza[statki[0][statki->size() - i]][statki[1][statki->size() - i]] = a;
 	}
 }
 
 int main()
 {
+	srand(time(NULL));
 	const int n = 7;
-	int plansza1[n][n];
+	int plansza1[n][n] = { 0 };
+	int plansza2[n][n] = { 0 };
+	int komputer[25][2] = { 0 };
+
+
 	string plansza11[n][n];
+	
 	vector <int> statki1[3];
-	int zatopiony = 0;
+	vector <int> statki2[3];
+
+	int zatopiony = 0, licznik = 1;
 
 	//zerowania tablic
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++) {
-			plansza1[i][j] = 0; //Y X
 			plansza11[i][j] = ".";
+			komputer[licznik][0] = i + 1;
+			komputer[licznik][1] = j + 1;
+			licznik++;
+			//plansza1[i][j] = 0; //Y X
+			//plansza2[i][j] = 0; //Y X
 		}
 	for (int i = 0; i < 3; i++) statki1[i].push_back(0);
+	for (int i = 0; i < 3; i++) statki2[i].push_back(0);
 
-	umiesc_statek(3, plansza1, statki1);
-	umiesc_statek(2, plansza1, statki1);
-	umiesc_statek(1, plansza1, statki1);
+	//umiesc_statek(3, plansza1, statki1);
+	//umiesc_statek(2, plansza1, statki1);
+	//umiesc_statek(1, plansza1, statki1);
+
+	umiesc_statek_k(3, plansza2, statki2);
+	umiesc_statek_k(2, plansza2, statki2);
+	umiesc_statek_k(1, plansza2, statki2);
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
@@ -89,54 +224,60 @@ int main()
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++)
-			cout << plansza11[j][i] << " ";
+			cout << plansza2[j][i] << " ";
 		cout << endl;
 	}
 
 	while (zatopiony < 3) {
-		int x, y,id;
+		int x, y, z,los;
 		cin >> x >> y;
-		if ((plansza1[x][y] <= 3) && (plansza1[x][y] >= 1)) {
-			if (plansza1[x][y] == 1) {
-				plansza1[x][y] = 0;
-					zatopiony++;
-					cout << "zatopiony\n";
-			}
-			else {
-				for (int i = 1; i < statki1[2].size() - 1; i++) 
-					if (statki1[0][i] == x && statki1[1][i] == y) {
-						id = statki1[2][i];
-						statki1[0][i] = 0;
-						statki1[1][i] = 0;
-						statki1[2][i] = 0;
-						plansza1[x][y] = 0;
-					}
-				
-				for (int i = 1; i < statki1[2].size() - 1; i++)
-					if (statki1[2][i] == id) {
-						plansza1[statki1[0][i]][statki1[1][i]]--;
-					}
-				cout << "trafiony\n";
-			}
-			plansza11[x][y] = "X";
+		z = strzal(x, y, plansza2, statki2);
+
+		if (z == 2) {
+			cout << "Zatopiony\n";
+			plansza11[x][y] = 'X';
+			zatopiony++;
 		}
-		else plansza11[x][y] = "O";
-	cout << endl;
+		else if (z == 1) {
+			cout << "Trafiony\n";
+			plansza11[x][y] = 'X';
+		}
+		else plansza11[x][y] = 'O';
+
 	for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++)
 				cout << plansza11[j][i] << " ";
 			cout << endl;
 	}
+
+	los = rand() % licznik;
+	x = komputer[los][0];
+	y = komputer[los][1];
+	z = strzal(x, y, plansza1, statki1);
+	komputer[los][0] = 9;
+	komputer[los][1] = 9;
+	wstawianie_sort(komputer, 25);
+
+	if (z == 2) {
+
+	}
+	else if (z == 1) {
+
+	}
+	else {
+
+	}
+
 	}
 }
 
 
-//*/
+
 
 /*
 #include <iostream>
-#include <string>
-
+//#include <string>
+//#include <vector>
 //NOTE Okazuje się że 5x5 tablica jest troche mała
 
 
@@ -148,7 +289,8 @@ int main()
 
 2 - przyległe pola (ale w rzeczywistości ma być 9 potem)
 8 - strzał
-//
+
+
 
 struct statekTyp { // struktura statku
 	int x, y;
@@ -160,9 +302,11 @@ struct statekTyp { // struktura statku
 using namespace std;
 
 char statki_symbole[] = {
-	'0', // Puste pole
+	'.', // Puste pole
 	'#', // Statek
-	'*'  // Pole przyległe (nie można tu wstawiać)
+	'*', // Pole przyległe (nie można tu wstawiać)
+	'O', // Pudło
+	'X', // Strzał trafiony
 };
 
 void show(int p[][5]) { // p od Pole
@@ -176,35 +320,71 @@ void show(int p[][5]) { // p od Pole
 	}
 }
 
+int ile_niezatopionych(statekTyp statki[], int n = 3) {
+	int res = 0;
+	for (int i = 0; i < n; i++)
+		res += !(statki[i].trafienia == statki[i].len);
+	return res;
+}
+
 void wstaw_statek(statekTyp s, int p[][5]) {
 	if (s.orient == 0) { // poziomo
-		if (s.x - 2 >= 0) 		 p[s.y - 1][s.x - 2] = 2; 	// przyległe pole na lewo od statku
-		if (s.x + s.len <= 5) p[s.y - 1][s.x + s.len - 1] = 2; 	// przyległe pole na prawo od statku
 
-		for (int i = 0; i < s.len; i++) { 				  // Wstawienie pól przyległych nad i pod statkiem
-			if (s.y - 2 >= 0) p[s.y - 2][s.x - 1 + i] = 2;
-			if (s.y < 5)	   p[s.y][s.x - 1 + i] = 2;
-		}
+		for (int i = s.x - 2; i < s.x + s.len; i++)
+			for (int j = s.y - 2; j < s.y + 1; j++)
+				if (i >= 0 && i < 5 && j >= 0 && j < 5)	p[j][i] = 2;
 
 		for (int i = 0; i < s.len; i++)	p[s.y - 1][i + s.x - 1] = 1; // wstaw tam gdzie jest statek
 	}
 	else { 			   // pionowo
-		if (s.y - 2 >= 0) p[s.y - 2][s.x - 1] = 2; 				  // przyległe pole na górze od statku
-		if (s.y + s.len <= 5) p[s.y + s.len - 1][s.x - 1] = 2; // przyległe pole na dole od statku
 
-		for (int i = 0; i < s.len; i++) { 				    // Wstawienie pól przyległych po bokach statku
-			if (s.x - 2 < 5 && s.x != 1)  p[s.y - 1 + i][s.x - 2] = 2; //! != 1 jest po to żeby przyległe pola się nie pojawiły po drugiej stronie
-			if (s.x >= 0 && s.x != 5)  p[s.y - 1 + i][s.x] = 2; //! != 5 Tutaj to samo, przyległe pola pojawiają się po drugiej stronie, a != 5 powoduje że nie wstawiają się gdy statek jest na końcu pola
-		}
+		for (int i = s.x - 2; i < s.x + 1; i++)
+			for (int j = s.y - 2; j < s.y + s.len; j++)
+				if (i >= 0 && i < 5 && j >= 0 && j < 5)	p[j][i] = 2;
 
 		for (int i = 0; i < s.len; i++)	p[i + s.y - 1][s.x - 1] = 1; // wstaw tam gdzie jest statek
+	}
+}
+
+int znajdz_statek(statekTyp statki[], int x, int y, int n = 3) {
+	for (int i = 0; i < n; i++) {
+		if (statki[i].orient == 0) { // statek poziomy
+
+			if (statki[i].y == y)
+				for (int j = 0; j < statki[i].len; j++) if (statki[i].x + j == x) return i;
+		}
+		else { // statek pionowy
+
+			if (statki[i].x == x)
+				for (int j = 0; j < statki[i].len; j++) if (statki[i].y + j == y) return i;
+		}
+	}
+	return -1;
+}
+
+void strzel(int x, int y, statekTyp statki[], int p[][5]) {
+	int index = znajdz_statek(statki, x, y);
+	if (index == -1) {
+		cout << "\nPUDŁO\n";
+		p[y - 1][x - 1] = 3;
+	}
+	else {
+		if (statki[index].len == statki[index].trafienia || p[y - 1][x - 1] >= 3) cout << "\nPUDŁO\n"; // Jeżeli statek już wcześniej trafiony
+		else {
+			statki[index].trafienia++;
+			if (statki[index].len == statki[index].trafienia)
+				cout << "\n!!ZATOPIONY!!\n"; // Zatopiony jeżeli wszystkie pola już zostały trafione
+			else
+				cout << "\nTRAFIONY\n";
+			p[y - 1][x - 1] = 4;
+		}
 	}
 }
 
 int main() {
 
 	int ile_statkow = 3; // ile statków dla gracza
-	int* statki_lista = new int[ile_statkow]{ 3,2,1 }; // Trzyma ile i jakie statki chcemy w grze (czyli ich długość)
+	int* statki_lista = new int[ile_statkow] {3, 2, 1}; // Trzyma ile i jakie statki chcemy w grze (czyli ich długość)
 
 
 	statekTyp* statki_gracz1 = new statekTyp[ile_statkow];
@@ -213,7 +393,6 @@ int main() {
 	int pole[5][5] = { 0 }; // powinno być pole1 i pole2, żeby mieć strzały gracza1 i gracza2
 
 	for (int i = 0; i < ile_statkow; i++) { // Pętla wstawiania danych o statku
-		//? Czy pokazywać pole z każdym wstawieniem statku
 		show(pole);
 
 		int x, y, z = 0;
@@ -258,12 +437,31 @@ int main() {
 		statki_gracz1[i].orient = z;
 		statki_gracz1[i].len = len;
 
-		// Tu ustawić te pola w okół statku na pola przyległe (nie można stawiać statku obok drugiego statku)
+
 		// I wstawić statek na pole
 		wstaw_statek(statki_gracz1[i], pole);
 	}
 
-	show(pole);
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			if (pole[i][j] == 2) pole[i][j] = 0; // Wyczyść przyległe pola
+
+	while (ile_niezatopionych(statki_gracz1) > 0) {
+		show(pole);
+		int x, y;
+		cout << "\nGdzie strzelic?\nx: ";
+		cin >> x;
+		cout << "y: ";
+		cin >> y;
+
+		if ((x <= 5 && x >= 1) && (y >= 1 && y <= 5)) {
+			strzel(x, y, statki_gracz1, pole);
+		}
+		else {
+			cout << "Miejsce poza polem, spróbuj jeszcze raz\n";
+		}
+	}
+	cout << "\n\n PRZEGRAŁEŚ \n\n";
 	return 0;
 }
 */
